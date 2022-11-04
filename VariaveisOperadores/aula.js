b81f26f70cb02b4fe05cac54d301ebd290474ae7
Guilherme Cruz <67871173+cruz-g-m@users.noreply.github.com>
2022-11-04 07:00:51 -0300

    Adicionando variaveis e operadores

---
+++ VariaveisOperadores/aula.js
@@ -0,0 +1,10 @@
+
+let variavel = 10 + 10;
+                    // pode ser alterada.
+//variavel = 20;
+
+const pi = 3.14; // é uma constante, não se pode alterar o valor.
+
+
+
+console.log(variavel);
---
+++ VariaveisOperadores/desafio.js
@@ -0,0 +1,7 @@
+let precoCombustivel = 5.79;
+let KmPorLitros = 10;
+let distanciaEmKm = 100;
+
+let totalGasto = (distanciaEmKm / KmPorLitros) * precoCombustivel;
+
+console.log(totalGasto);
